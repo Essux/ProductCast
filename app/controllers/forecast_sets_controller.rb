b4659d9d72af81cbff8092a9bd1b005b7e3f3713
Essux <jjsuarestra99@gmail.com>
2017-10-29 22:02:35 -0500

    Adding model calls

--- app/controllers/forecast_sets_controller.rb
+++ app/controllers/forecast_sets_controller.rb
@@ -2,7 +2,7 @@ class ForecastSetsController < ApplicationController
   def to_param
     id
   end
-  
+
   def select_models
     @forecast_set = ForecastSet.new
   end
@@ -11,20 +11,34 @@ class ForecastSetsController < ApplicationController
     @forecast_set = ForecastSet.new(forecast_set_models)
     @applied_parameters = AppliedParameter.new
   end
-  
+
+  # Este método es todo ProductCast
   def show
     @forecast_set = ForecastSet.new(forecast_set_params)
     @forecast_set.save
     params[:model_ids]
     params[:model_ids].drop(1).each { |model|
       model = Model.find(model)
+      execution = Execution.find_by(model_id: model.id, forecast_set_id: @forecast_set.id)
+      # Parámetros de la instancia de Modelo que se va a ejecutar
+      model_params = {}
       model.parameters.each { |parameter|
-        execution = Execution.find_by(model_id: model.id, forecast_set_id: @forecast_set.id)
+        model_params[parameter.name] = params[:forecast_set][:applied_parameters][:"#{parameter.id}"]
         unless execution == nil
           applied_parameter = AppliedParameter.new(parameter_id: parameter.id, execution_id: execution.id, value: params[:forecast_set][:applied_parameters][:"#{parameter.id}"])
           applied_parameter.save
         end
       }
+      # Nombre de la subclase de Modelo que se va a usar
+      model_class_name = model.class_name
+      # Crea una instancia de la clase de Modelo según su nombre
+      model_instance = model_class_name.constantize.new(model_id, model.name, model_params)
+      # Trae los datos para hacer predicciones
+      historical_data = Product.get_historical_data(@forecast_set.product_id)
+      # Ejecuta el modelo
+      predicted_data = model_instance.run(historical_data, @forecast_set.forecast_amount)
+      # Guarda las predicciones en la base de datos
+      Forecast.savePredictedData(predicted_data, execution)
     }
   end
 
@@ -33,7 +47,7 @@ private
     def forecast_set_models
       params.require(:forecast_set).permit(:model_ids => [])
     end
-    
+
     def forecast_set_params
       params.require(:forecast_set).permit(:forecast_amount).tap { |x|
         x[:product_id] = params[:product_id]
